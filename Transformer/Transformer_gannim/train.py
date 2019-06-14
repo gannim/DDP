layer normalization bugfix

Transformer/Transformer_gannim/train.py
@@ -18,12 +18,13 @@ CHECKPOINT_EVERY = 1000
 
 filename = 'data.txt'
 uc_data = dataHelper(filename)
-batch_size = 50
-n_hidden = 128
+batch_size = 64
+n_hidden = 512
 epochs = 100
 n_class = len(uc_data.tot_word_idx_dic)
 ##
 learning_rate = 0.001
+#learning_rate = 0.1
 keep_prob = 0.5
 heads_size = 8
 layer_size = 4
@@ -111,6 +112,7 @@ def run_train():
 
     with tf.Session(config=config) as sess:
         gstep = tf.Variable(0, name="gstep", trainable=False)
+        #learning_rate = tf.train.exponential_decay(0.1, gstep, 100000, 0.96, staircase=True)
         optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
         grads_vars = optimizer.compute_gradients(transformer.loss)
         train_op = optimizer.apply_gradients(grads_vars, global_step=gstep)

Transformer/Transformer_gannim/transformer.py
@@ -36,6 +36,7 @@ class Transformer(object):
         self.position_encoded = tf.nn.embedding_lookup(self.pos_encodings, self.positional_inputs)
         self.enc_input_pos = self.enc_input_embeddings + self.position_encoded
         self.dec_input_pos = self.dec_input_embeddings + self.position_encoded 
+        ## enc_input_pos, dec_input_pos 에 dropout 을 먹이기도 하는듯 ?
 
         ## encoding layer 
         with tf.variable_scope('encode'):
@@ -112,16 +113,19 @@ class Transformer(object):
         
     def sublayer_connection(self, inputs, sublayer, dropout):
         # LayerNorm(x + Sublayer(x))
-        return tf.layers.dropout(self.layer_norm(inputs + sublayer), dropout)
+        #return tf.layers.dropout(self.layer_norm(inputs + sublayer), dropout)
+        # Residual connection = input + sublayer 
+        return self.layer_norm(inputs + sublayer)
 
     @staticmethod
-    def layer_norm(inputs, eps=1e-6):
+    def layer_norm(inputs, epsilon=1e-8): #epsilon=1e-6):
         feature_shape = inputs.get_shape()[-1:]
-        mean = tf.keras.backend.mean(inputs, [-1], keepdims=True)
-        std = tf.keras.backend.std(inputs, [-1], keepdims=True)
+        mean, variance = tf.nn.moments(inputs, [-1], keep_dims=True)
         beta = tf.Variable(tf.zeros(feature_shape), trainable=False)
         gamma = tf.Variable(tf.ones(feature_shape), trainable=False)
-        return gamma * (inputs - mean) / (std + eps) + beta
+        #normalized = (inputs - mean) / ((variance + epsilon) ** 0.5) 
+        normalized = (inputs - mean) / (variance + epsilon)
+        return gamma * normalized + beta
 
     @staticmethod
     def scale_dot_product_attention(query, key, value, masked=False):
@@ -140,10 +144,15 @@ class Transformer(object):
 
     @staticmethod
     def feed_forward(inputs, num_units, dropout):
+        # Position-wise Feed-Forward Networks
         # FFN(x) = max(0, xW1 + b1)W2 + b2 
         with tf.variable_scope("feed_forward", reuse=tf.AUTO_REUSE):
+            # relu ==  max(0, x)
+            # max(0, xW1 + b1)
+            ## inner layer
             outputs = tf.layers.dense(inputs, num_units[0], activation=tf.nn.relu)
-            outputs = tf.layers.dropout(outputs, dropout)
+            #outputs = tf.layers.dropout(outputs, dropout)
+            # outer layer 
             return tf.layers.dense(outputs, num_units[1])
 
     @staticmethod
@@ -158,4 +167,9 @@ class Transformer(object):
         # PE(pos,2i+1)
         encoded_vec[1::2] = np.cos(encoded_vec[1::2]) 
         return tf.convert_to_tensor(encoded_vec.reshape([sentence_length, dims]), dtype=dtype)
+    
+    @staticmethod 
+    def noam_scheme(init_lr, gstep, warmup_step=4000):
+        step = tf.cast(gstep + 1, dtype=tf.float32)
+        return init_lr * warmup_steps ** 0.5 * tf.minimum(step *warmup_steps ** -1.5, step ** -0.5)
 